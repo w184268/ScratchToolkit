dee617a2b4305dedef68f78d53eb8ff52f384e4e
Eric学编程 <1843794003@qq.com>
2024-12-15 16:06:51 +0800

    Update stp.py

--- stp.py
+++ stp.py
@@ -27,7 +27,7 @@ if __name__=='__main__':
     try:
         parser=ap.ArgumentParser(description="The command list of Scratch-To-Python")
         parser.add_argument('file_path',type=str,default=None,help='Your .sb3 file.')
-        parser.add_argument('--mode','-m',dest="mode",choices=['path','name'],default="path",help='The type of <file_path>.Choose from "path" and "name".')
+        parser.add_argument('--mode','-m',dest="mode",choices=['path','name'],default="path",help='The type of <file_path>.')
         parser.add_argument('--run','-r',dest="run",action="store_true",default=False,help="Run and check the output file.")
         parser.add_argument('--no-log','-nl',dest="no_log",action="store_true",default=False,help="Do not show all the log.")
         args=parser.parse_args()
