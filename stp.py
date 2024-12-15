15a46097aea3f35ef8ba229792876383aa6645a0
Eric学编程 <1843794003@qq.com>
2024-12-15 16:00:49 +0800

    fix

--- stp.py
+++ stp.py
@@ -30,14 +30,15 @@ Scratch-To-Pygame(Beta v0.0.1) is running!
 if __name__=='__main__':
     try:
         parser=ap.ArgumentParser(description="The command list of Scratch-To-Python")
-        parser.add_argument('file_path',type=str,help='Your .sb3 file.')
+        parser.add_argument('file_path',type=str,default=None,help='Your .sb3 file.')
         parser.add_argument('--mode','-m',dest="mode",choices=['path','name'],default="path",help='The type of <file_path>.Choose from "path" and "name".')
         parser.add_argument('--run','-r',dest="run",action="store_true",default=False,help="Run and check the output file.")
         parser.add_argument('--no-log','-nl',dest="no_log",action="store_true",default=False,help="Do not show all the log.")
         args=parser.parse_args()
         if args.no_log:
             log.remove()
-        main(args.file_path,args.mode=='path',args.run)
+        if args.file_path:
+            main(args.file_path,args.mode=='path',args.run)
     except BaseException:
-        log.error(traceback.format_exc())
+        log.error('\n'+traceback.format_exc())
         exit(1)
