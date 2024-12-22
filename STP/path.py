a8d92449d83a62df6a83c6851804e02e7d374ace
Eric学编程 <1843794003@qq.com>
2024-12-22 17:00:39 +0800

    if

--- STP/path.py
+++ STP/path.py
@@ -7,21 +7,23 @@ from cairosvg import svg2png
 from PIL import Image
 
 class PathTool:
-    def __init__(self,fp:str|tuple[str],mode='p'):
-        '''when mode is p,that means fp is a path; 
-           when mode is n,that means fp is a file name.'''
-        if isinstance(fp,str): fp=os.path.normpath(fp)
-        log.debug("Using the PathTool...")
-        match mode:
-            case 'p':
-                self.DIR=os.path.dirname(fp)
-                self.FILE=os.path.basename(fp)
-                self.NAME,self.SUFFIX=os.path.splitext(self.FILE)
-            case 'n':
-                self.FILE=fp
-                self.NAME,self.SUFFIX=os.path.splitext(fp)
-            case 'j':
-                self.j=os.path.join(*(os.path.normpath(p) for p in fp))
+    def __init__(self,fp:str|tuple[str]=None,mode='p'):
+        '''mode = 'p': fp是一个文件路径;    
+           mode = 'n': fp是一个文件名;  
+           mode = 'j': 只合并路径，fp的类型为tuple[str]。'''
+        if fp:
+            if isinstance(fp,str): fp=os.path.normpath(fp)
+            log.debug("Using the PathTool...")
+            match mode:
+                case 'p':
+                    self.DIR=os.path.dirname(fp)
+                    self.FILE=os.path.basename(fp)
+                    self.NAME,self.SUFFIX=os.path.splitext(self.FILE)
+                case 'n':
+                    self.FILE=fp
+                    self.NAME,self.SUFFIX=os.path.splitext(fp)
+                case 'j':
+                    self.j=os.path.join(*(os.path.normpath(p) for p in fp))
 
     def join(self,args:tuple[str]=()):
         if hasattr(PathTool,'j'):
@@ -86,8 +88,23 @@ class PackingScratch3File:
         
         :param dp: .sb3文件解包后文件夹位置
         """
-        if os.path.isdir(dp) and 'project.json' in os.listdir(dp):
+        if self.is_sb3_dir(dp):
             log.debug(f"Packing from {dp}...")
         else:
             log.error(f"{dp} is not a dir unpacked from a .sb3 file!")
             exit(1)
+    def is_sb3_dir(self,dp:str):
+        result=True
+        if os.path.isdir(dp) and 'project.json' in os.listdir(dp):
+            t=PathTool((dp,'project.json'),'j')
+            with open(t.join(dp),'r',encoding='utf-8') as f:
+                f:dict=json.load(f)
+            for i in ('targets','monitors','extensions','meta'):
+                if not f.get(i,[]):
+                    result=False
+                    break
+                elif result != False:
+                    result=True
+        else:
+            result=False
+        return result
