e46496f3f43fb1819b10ddd863255cd46f8a5a67
Eric学编程 <1843794003@qq.com>
2025-01-31 16:28:12 +0800

    rm __functool

--- src/__STP/__old.py
+++ src/__STP/__old.py
@@ -15,4 +15,30 @@ class Function():
         args=self.args[sp][func]
         for e,a in zip(exec,args):
             e(*a)
+
+class CodeParser():
+    def __functool(self,mutation:dict,args:Union[str,Tuple[str,...]]="",func=False,free=False):
+        type={'%s':'int|float|str','%b':'bool'}
+        name=[]
+        argtypes=[]
+        c=mutation['proccode'].split(' ')
+        for i in range(len(c)):
+            if c[i].isidentifier():
+                if i==0 or not c[i-1].isidentifier():
+                    name.append(c[i])
+                else:
+                    name[-1]+=c[i]
+            else:
+                argtypes.append(c[i])
+
+        funcname='_'+'_'.join(name)
+        if funcname not in self.funccode: #创建函数
+            self.funccode[funcname]=[{},{}]
+        for argname,argdefault,argtype in zip(eval(mutation['argumentnames']),eval(mutation['argumentdefaults']),argtypes):
+            self.funccode[funcname][0][argname.replace(' ','_')]=[argdefault,type.get(argtype,'Any')] #type: ignore 
+        if not func:
+            if not free:
+                self.funccode[funcname][1]['self.'+self.opcode+'('+', '.join(args)+')']=self.depth
+            else:
+                self.funccode[funcname][1][args]=self.depth
 '''
--- src/__STP/core.py
+++ src/__STP/core.py
@@ -146,30 +146,6 @@ class CodeParser:
                 self.funccode['__init__'][1][string]=0 #角色基础信息，无需深度
             case 4:
                 self.gamecode.append('        '+string)
-    def __functool(self,mutation:dict,args:Union[str,Tuple[str,...]]="",func=False,free=False):
-        type={'%s':'int|float|str','%b':'bool'}
-        name=[]
-        argtypes=[]
-        c=mutation['proccode'].split(' ')
-        for i in range(len(c)):
-            if c[i].isidentifier():
-                if i==0 or not c[i-1].isidentifier():
-                    name.append(c[i])
-                else:
-                    name[-1]+=c[i]
-            else:
-                argtypes.append(c[i])
-
-        funcname='_'+'_'.join(name)
-        if funcname not in self.funccode: #创建函数
-            self.funccode[funcname]=[{},{}]
-        for argname,argdefault,argtype in zip(eval(mutation['argumentnames']),eval(mutation['argumentdefaults']),argtypes):
-            self.funccode[funcname][0][argname.replace(' ','_')]=[argdefault,type.get(argtype,'Any')] #type: ignore 
-        if not func:
-            if not free:
-                self.funccode[funcname][1]['self.'+self.opcode+'('+', '.join(args)+')']=self.depth
-            else:
-                self.funccode[funcname][1][args]=self.depth
 
     def get_nested_depth(self,id:str,block:dict,depth=0):
         """
