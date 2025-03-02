95e0462c1d93bbeb65f67d90fcb00d2a9727277f
Eric学编程 <1843794003@qq.com>
2025-03-02 16:07:25 +0800

    up

--- src/__STP/codeparser.py
+++ src/__STP/codeparser.py
@@ -75,30 +75,6 @@ class CodeParser(Data):
     def add(self): #积木管理
         type_=f"{self.classname} -> {self.id}"
         log.debug(f'Converting {type_} (name="{self.opcode}" ,depth={self.depth})...')
-        match self.opcode: #匹配相应的积木名
-            case "control_wait":
-                self.fstr(args=(self.idinfo['inputs']['DURATION'][1][1]))
-            case "control_repeat":
-                '''self.fstr(f"for _ in range({self.idinfo['inputs']['TIMES'][1][1]}):",3)'''
-            case "control_forever":
-                self.fstr("while True:",3)
-            case "control_if":
-                self.funccode['__init__'][1][f"id={self.id}"]=self.depth
-                '''self.fstr(f"if {self.idinfo['inputs']['CONDITION'][1][1]}:",3)'''
-            case "control_if_else":
-                '''self.fstr(f"if {self.idinfo['inputs']['CONDITION'][1][1]}:",3)'''
-                self.fstr("else:",3)
-            case "operator_add":
-                self.fstr(args=["NUM",'+'],mode=7)
-            case "operator_subtract":
-                self.fstr(args=["NUM",'-'],mode=7)
-            case "operator_equals":
-               self.fstr(args=["OPERAND",'=='],mode=7)
-            case "procedures_definition":
-                self.fstr(self.blocks[self.idinfo['inputs']['custom_block'][1]]['mutation'],1)
-            case _:
-                if self.opcode not in USERSET["blocks"]['ignore']:
-                    log.warning(f'Unknown id "{self.opcode}"!')
 
     def fstr(self,string:str|dict="",mode=0,args=()):
         '''
---
+++ src/__STP/reg.py
@@ -0,0 +1,29 @@
+from .codeparser import *
+
+class CodeParser(CodeParser):
+    def add(self):
+        super().add()
+        match self.opcode: #匹配相应的积木名
+            case "control_wait":
+                self.fstr(args=(self.idinfo['inputs']['DURATION'][1][1]))
+            case "control_repeat":
+                '''self.fstr(f"for _ in range({self.idinfo['inputs']['TIMES'][1][1]}):",3)'''
+            case "control_forever":
+                self.fstr("while True:",3)
+            case "control_if":
+                self.funccode['__init__'][1][f"id={self.id}"]=self.depth
+                '''self.fstr(f"if {self.idinfo['inputs']['CONDITION'][1][1]}:",3)'''
+            case "control_if_else":
+                '''self.fstr(f"if {self.idinfo['inputs']['CONDITION'][1][1]}:",3)'''
+                self.fstr("else:",3)
+            case "operator_add":
+                self.fstr(args=["NUM",'+'],mode=7)
+            case "operator_subtract":
+                self.fstr(args=["NUM",'-'],mode=7)
+            case "operator_equals":
+               self.fstr(args=["OPERAND",'=='],mode=7)
+            case "procedures_definition":
+                self.fstr(self.blocks[self.idinfo['inputs']['custom_block'][1]]['mutation'],1)
+            case _:
+                if self.opcode not in USERSET["blocks"]['ignore']:
+                    log.warning(f'Unknown id "{self.opcode}"!')
